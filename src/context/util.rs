f6e7b7a4705977affbbc46a269c283e8e85f8481
Christofer Nolander <christofer.nolander@gmail.com>
2021-04-01 14:28:42 +0200

    utility functions

---
+++ src/context/util.rs
@@ -0,0 +1,47 @@
+use super::GpuContext;
+
+pub(crate) fn copy_entire_texture_to_texture(
+    source: &wgpu::Texture,
+    destination: &wgpu::Texture,
+    size: crate::Size,
+    encoder: &mut wgpu::CommandEncoder,
+) {
+    encoder.copy_texture_to_texture(
+        wgpu::TextureCopyView {
+            texture: source,
+            mip_level: 0,
+            origin: wgpu::Origin3d::ZERO,
+        },
+        wgpu::TextureCopyView {
+            texture: destination,
+            mip_level: 0,
+            origin: wgpu::Origin3d::ZERO,
+        },
+        wgpu::Extent3d {
+            width: size.width,
+            height: size.height,
+            depth: 1,
+        },
+    );
+}
+
+pub(crate) fn create_texture(
+    size: crate::Size,
+    format: wgpu::TextureFormat,
+    usage: wgpu::TextureUsage,
+    gpu: &GpuContext,
+) -> wgpu::Texture {
+    gpu.device.create_texture(&wgpu::TextureDescriptor {
+        label: None,
+        size: wgpu::Extent3d {
+            width: size.width,
+            height: size.height,
+            depth: 1,
+        },
+        mip_level_count: 1,
+        sample_count: 1,
+        dimension: wgpu::TextureDimension::D2,
+        format,
+        usage,
+    })
+}
